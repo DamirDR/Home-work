a2348ab5fae56a9d27ab76b3184ecf09ead02e67
Damir <SahibillinDR@yandex.ru>
2022-08-09 00:00:54 +0300

    Home work Seminar01

--- Seminar01/task02/Program.cs
+++ Seminar01/task02/Program.cs
@@ -3,6 +3,7 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+
 Console.WriteLine("Введите первое число ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
